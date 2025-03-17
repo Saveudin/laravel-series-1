97afc84b8ad083cc4dfc4fc81eb468efdc7e116a
Dodi <ronthunder40@gmail.com>
2025-03-17 14:58:59 +0700

    database dan migrasi

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,9 +7,9 @@
             <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
 
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a> | 13 March 2025
+            <a href="#">{{ $post['author'] }}</a> | {{ $post['created_at']->diffForHumans() }}
         </div>
-        <p class="my-4 font-light">{{ $post['body'] }}</p>
+        <p class="my-4 font-light text-justify">{{ $post['body'] }}</p>
         <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo;Back</a>
         </article>
 
--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,23 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use PharIo\Manifest\Author;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PenggunaController;
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/posts', function() {
-    return view('posts', ['title' => 'Blog', 'posts' => [
-        [
-            'id' => 1,
-            'title' => 'Judul Artikel 1',
-            'author' => 'Misono Mika',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, ipsam distinctio temporibus officia repellat autem. Quos dolore doloribus ex, nulla consectetur tempora sint cum pariatur quia aspernatur, minus aut ratione!'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Judul Artikel 2',
-            'author' => 'Onikata Kayoko',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint recusandae eveniet consequatur quaerat, iusto sunt vel possimus doloremque esse, dignissimos quisquam iure consectetur odit dolorem. Repellendus quia accusantium est numquam?'
-        ]
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
 Route::get('/posts/{id}', function($id) {
-    $posts = [[
-        'id' => 1,
-        'title' => 'Judul Artikel 1',
-        'author' => 'Misono Mika',
-        'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, ipsam distinctio temporibus officia repellat autem. Quos dolore doloribus ex, nulla consectetur tempora sint cum pariatur quia aspernatur, minus aut ratione!'
-    ],
-    [
-        'id' => 2,
-        'title' => 'Judul Artikel 2',
-        'author' => 'Onikata Kayoko',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint recusandae eveniet consequatur quaerat, iusto sunt vel possimus doloremque esse, dignissimos quisquam iure consectetur odit dolorem. Repellendus quia accusantium est numquam?'
-    ]];
 
-    $post = Arr::first($posts, function($post) use ($id){
-        return $post['id'] == $id;
-    });
+    $post = Post::find($id);
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
